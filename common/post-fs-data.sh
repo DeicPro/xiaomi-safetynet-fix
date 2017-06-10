f960dc66e05b5e5f82bbafe750a99f486dd5294d
DeicPro <DeicPro@users.noreply.github.com>
2017-06-10 08:26:07 +0200

    Cleanup and added some dangerous props fixes

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -5,50 +5,26 @@ MODDIR=${0%/*}
 
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
-exec &> $MODDIR/xiaomi-safetynet-fix.log
+LOGFILE="/cache/magisk.log"
+
+exec &>> $LOGFILE
+
+echo "### Universal SafetyNet Fix ###"
 
 set -x
 
-LOGFILE="/cache/magisk.log"
-
-log_print() {
-    echo "Universal SafetyNet Fix: $1"
-    echo "Universal SafetyNet Fix: $1" >> $LOGFILE
-    log -p i -t "Universal SafetyNet Fix" "$1"
-}
-
-if [ -f "/sbin/magisk" ]; then RESETPROP="/sbin/magisk resetprop"
-elif [ -f "/data/magisk/magisk" ]; then RESETPROP="/data/magisk/magisk resetprop"
-elif [ -f "/magisk/.core/bin/resetprop" ]; then RESETPROP="/magisk/.core/bin/resetprop"
-elif [ -f "/data/magisk/resetprop" ]; then RESETPROP="/data/magisk/resetprop"
-else exit 1; fi
-
-set_prop() {
-    if [ "$4" ]; then MODEL="$4"; else MODEL="$DEVICE"; fi
-    $RESETPROP -v -n "ro.build.fingerprint" "Xiaomi/$MODEL/$DEVICE:$1/$2/$3:user/release-keys"
-    $RESETPROP -v -n "ro.bootimage.build.fingerprint" "Xiaomi/$MODEL/$DEVICE:$1/$2/$3:user/release-keys"
-    script_end &
-    exit
-}
-
-script_end() {
-    while :; do [ "$(getprop persist.magisk.hide)" == "1" ] && \
-    break || setprop "persist.magisk.hide" "1"; sleep 1; done
-    set +x
-    while :; do [ "$(getprop sys.boot_completed)" == "1" ] && \
-    [ "$(getprop init.svc.magisk_service)" == "stopped" ] && break; sleep 1; done
-    set -x
+get_logs() {
+    set +x; while :; do [ "$(getprop sys.boot_completed)" == "1" ] && \
+    [ "$(getprop init.svc.magisk_service)" == "stopped" ] && { set -x; break; }; sleep 1; done
     getprop
     sleep 1
     cat $LOGFILE
-    #check_safetynet &
-    #check_unmount
+    check_safetynet &
+    check_unmount
 }
 
 grep_logcat() {
-    set +x
-    while :; do logcat -b events -v raw -d | grep "$1" && break; sleep 1; done
-    set -x
+    set +x; while :; do logcat -b events -v raw -d | grep "$1" && { set -x; break; }; sleep 1; done
 }
 
 check_safetynet() {
@@ -56,101 +32,49 @@ check_safetynet() {
     grep_logcat "MANAGER: SN: Google API Connected"
     grep_logcat "MANAGER: SN: Check with nonce"
     grep_logcat "MANAGER: SN: Response"
-    #grep_logcat "MANAGER: StatusFragment: SafetyNet UI refresh triggered"
 }
 
 check_unmount() {
     echo "Waiting for MagiskHide unmount..."
-    set +x
-    while :; do grep "MagiskHide: Unmounted (/sbin)" "$LOGFILE" && \
-    grep "MagiskHide: Unmounted (/magisk)" "$LOGFILE" && break; sleep 1; done
-    set -x
+    set +x; while :; do grep "MagiskHide: Unmounted (/sbin)" "$LOGFILE" && \
+    grep "MagiskHide: Unmounted (/magisk)" "$LOGFILE" && { set -x; break; }; sleep 1; done
     sleep 1
     MAGISKHIDE_LOG=$(grep -n -x "* Starting MagiskHide" "$LOGFILE")
-    /data/magisk/busybox tail +${MAGISKHIDE_LOG%%:*} "$LOGFILE"
+    ${BUSYBOX}tail +${MAGISKHIDE_LOG%%:*} "$LOGFILE"
 }
 
-#logcat -b events -v raw -t 10
 #wget --post-data "api_option=paste&api_dev_key=2dc5d9876384c0232c6ce30ae0558479&api_paste_code=$(cat $log)&api_paste_name=$log" http://pastebin.com/api/api_post.php > /dev/null 2>&1
-#ro.build.selinux=0
-#ro.build.type=user
-#ro.build.tags=release-keys
-#selinux.reload_policy=1
+#$(cat /system/build.prop | sed -n "s/^$1=//p")
+#while :; do [ "$(getprop persist.magisk.hide)" == "1" ] && \
+#break || setprop "persist.magisk.hide" "1"; sleep 1; done
 
-DEVICE=$(cat /system/build.prop | sed -n "s/^ro.product.device=//p")
+if [ -d "/data/data/com.topjohnwu.magisk/busybox" ]; then BUSYBOX="/data/data/com.topjohnwu.magisk/busybox/"
+elif [ -f "/data/data/com.topjohnwu.magisk/busybox/busybox" ]; then BUSYBOX="/data/data/com.topjohnwu.magisk/busybox/busybox "
+elif [ -f "/data/app/com.topjohnwu.magisk-*/lib/*/libbusybox.so" ]; then BUSYBOX="/data/app/com.topjohnwu.magisk-*/lib/*/libbusybox.so "
+elif [ -d "/dev/busybox" ]; then BUSYBOX="/dev/busybox/"
+elif [ -f "/data/magisk/resetprop" ]; then BUSYBOX="/data/magisk/busybox "
+fi
 
-case $DEVICE in
-# Redmi Note 2
-    hermes) set_prop "5.0.2" "LRX22G" "V8.2.1.0.LHMCNDL";;
-# Redmi Note 3 MTK
-    hennessy) set_prop "5.0.2" "LRX22G" "V8.1.3.0.LHNCNDI";;
-# Redmi Note 3 Qualcomm
-    kenzo) set_prop "6.0.1" "MMB29M" "V8.2.1.0.MHOCNDL";;
-# Redmi Note 4 MTK
-    nikel) set_prop "6.0" "MRA58K" "V8.2.2.0.MBFCNDL";;
-# Mi 5
-    gemini) set_prop "6.0.1" "MXB48T" "V8.1.2.0.MAAMIDI";;
-# Mi 5s
-    capricorn) set_prop "6.0.1" "MXB48T" "V8.2.4.0.MAGCNDL";;
-# Mi 5s Plus
-    natrium) set_prop "6.0.1" "MXB48T" "V8.2.4.0.MBGCNDL";;
-# Mi MIX
-    lithium) set_prop "6.0.1" "MXB48T" "V8.2.3.0.MAHCNDL";;
-# Mi Max
-    hydrogen)set_prop "6.0.1" "MMB29M" "V8.2.3.0.MBCCNDL";;
-# Mi Max Prime
-    helium) set_prop "6.0.1" "MMB29M" "V8.2.3.0.MBDCNDL";;
-# Redmi 3S/Prime/3X
-    land) set_prop "6.0.1" "MMB29M" "V8.1.5.0.MALCNDI";;
-# Mi 4c
-    libra) set_prop "5.1.1" "LMY47V" "V8.2.1.0.LXKCNDL";;
-# Mi 5c
-    meri) set_prop "6.0" "MRA58K" "V8.1.15.0.MCJCNDI";;
-# Redmi Note 3 Special Edition
-    kate) set_prop "6.0.1" "MMB29M" "V8.1.3.0.MHRMIDI";;
-# Mi Note 2
-    scorpio) set_prop "6.0.1" "MXB48T" "V8.2.5.0.MADCNDL";;
-# Redmi Note 4X
-    mido) set_prop "6.0.1" "MMB29M" "V8.2.18.0.MCFCNDL";;
-# Redmi 2 Prime
-    wt88047) set_prop "5.1.1" "LMY47V" "V8.2.5.0.LHJCNDL";;
-# Redmi 2/4G
-    HM2014811) set_prop "4.4.4" "KTU84P" "V8.2.3.0.KHJCNDL" "2014811";;
-# Redmi 3/Prime
-    ido) set_prop "5.1.1" "LMY47V" "V8.1.3.0.LAIMIDI";;
-# Mi 4i
-    ferrari) set_prop "5.0.2" "LRX22G" "V8.1.5.0.LXIMIDI";;
-# Redmi 4
-    prada) set_prop "6.0.1" "MMB29M" "V8.1.5.0.MCECNDI";;
-# Redmi 4 Prime
-    markw) set_prop "6.0.1" "MMB29M" "V8.2.4.0.MBEMIDL";;
-# Redmi 4A
-    rolex) set_prop "6.0.1" "MMB29M" "V8.1.4.0.MCCMIDI";;
-# Mi Pad
-    mocha) set_prop "4.4.4" "KTU84P" "V8.2.2.0.KXFCNDL";;
-# Mi Note
-    virgo) set_prop "6.0.1" "MMB29M" "V8.1.4.0.MXEMIDI";;
-# Mi 3/Mi 4
-    cancro) set_prop "6.0.1" "MMB29M" "V8.1.6.0.MXDMIDI";;
-# Mi 2/2S
-    aries) set_prop "5.0.2" "LRX22G" "V8.1.3.0.LXAMIDI";;
-# Mi Pad 2
-    latte) set_prop "5.1" "LMY47I" "V8.2.2.0.LACCNDL";;
-# Mi Pad 3
-    cappu) set_prop "7.0" "NRD90M" "V8.2.8.0.NCICNEB";;
-# Mi 6
-    sagit) set_prop "7.1.1" "NMF26X" "V8.2.17.0.NCACNEC";;
-# Mi Note Pro
-    leo) set_prop "7.0" "NRD90M" "V8.2.3.0.NXHCNEC";;
-# Redmi 1
-    HM2013023) set_prop "4.4.2" "HM2013023" "V7.3.1.0.KHBCNDD" "2013023";;
-# Redmi 1S
-    armani) set_prop "4.4.4" "KTU84P" "V8.2.1.0.KHCMIDL";;
-# Redmi Pro
-    omega) set_prop "6.0" "MRA58K" "V8.1.2.0.MHQCNDI";;
-# Mi 4S
-    aqua) set_prop "5.1.1" "LMY47V" "V8.2.1.0.LAJCNDL";;
-# Redmi 4X
-    santoni) set_prop "6.0.1" "MMB29M" "V8.2.9.0.MAMMIEA";;
-    *) echo "Using Mi 6 prop for $DEVICE..."; DEVICE=sagit; set_prop "7.1.1" "NMF26X" "V8.2.17.0.NCACNEC";;
-esac
+RESETPROP="resetprop -v -n"
+
+if [ -f "/sbin/magisk" ]; then RESETPROP="/sbin/magisk $RESETPROP"
+elif [ -f "/data/magisk/magisk" ]; then RESETPROP="/data/magisk/magisk $RESETPROP"
+elif [ -f "/magisk/.core/bin/resetprop" ]; then RESETPROP="/magisk/.core/bin/$RESETPROP"
+elif [ -f "/data/magisk/resetprop" ]; then RESETPROP="/data/magisk/$RESETPROP"
+fi
+
+$RESETPROP "ro.build.fingerprint" "Xiaomi/sagit/sagit:7.1.1/NMF26X/V8.2.17.0.NCACNEC:user/release-keys"
+$RESETPROP "ro.bootimage.build.fingerprint" "Xiaomi/sagit/sagit:7.1.1/NMF26X/V8.2.17.0.NCACNEC:user/release-keys"
+$RESETPROP "ro.build.type" "user"
+$RESETPROP "ro.build.tags" "release-keys"
+$RESETPROP "ro.build.selinux" "0"
+$RESETPROP "selinux.reload_policy" "1"
+$RESETPROP "persist.magisk.hide" "1"
+
+#get_logs &
+
+set +x
+
+echo "### END ###"
+
+exit
